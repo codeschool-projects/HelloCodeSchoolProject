8cd39c6e172383f187c0a5fbd664e08882f88139
Adam Fortuna <adam@fortuna.name>
2016-08-30 07:55:30 -0400

    Updated tests to include tagging and add error messages

--- test/index_test.js
+++ test/index_test.js
@@ -17,19 +17,33 @@ describe('Your HTML Page', function() {
     );
   });
 
-  it('should have a different title', function() {
-    assert.notEqual(window.$('title').text(), 'Hello Code School');
+  it('should have a title @title', function() {
+    assert.equal(window.$('title').length, 1, 'Make sure to create a `title` element.');
   });
 
-  it('should have a different h1', function() {
-    assert.notEqual(window.$('h1').length, 0);
+  it('should have a title with the text "Code School" @title', function() {
+    assert.notEqual(window.$('title').text(), '', 'Make sure to set the content of the `title` element to your Code School username.');
   });
 
-  it('should have a ul', function() {
-    assert.notEqual(window.$('ul').length, 0);
+  it('should have a h1 element @h1', function() {
+    assert.isAtLeast(window.$('h1').length, 1, "Make sure to create an `h1` element.");
   });
 
-  it('should have at least 2 li elements', function() {
-    assert.isAtLeast(window.$('li').length, 2);
+  it('should have content in the h1 element @h1', function() {
+    assert.equal(window.$('h1').text(), 'Hello, Code School!', "Make sure to set the content of your `h1` element to 'Hello, Code School!'.");
+  });
+
+  it('should have a ul @ul', function() {
+    assert.isAtLeast(window.$('ul').length, 1, "Make sure to create a `ul` element.");
+  });
+
+  it('should have at least 2 li elements @li', function() {
+    assert.isAtLeast(window.$('li').length, 2, "Make sure to create at least 2 `li` elements.");
+  });
+
+  it('should have content for all `li` elements. @li', function() {
+    var message = "Make sure to include something you want to learn for each `li` element."
+    assert.notEqual(window.$('li:first').text(), '', message);
+    assert.notEqual(window.$('li:last').text(), '', message);
   });
 });
